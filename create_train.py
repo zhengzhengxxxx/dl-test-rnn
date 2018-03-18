b72c7f01ee80049b9800d507eabc4a23ef4cc4b8
zhengzhengxxxx <284067817@qq.com>
2018-03-18 20:02:39 +0800

    1
    1

--- create_train.py
+++ create_train.py
@@ -6,41 +6,39 @@ MODE = "TRANSLATE_ZH"    # 将MODE设置为"PTB_TRAIN", "PTB_VALID", "PTB_TEST",
 TARGET_DIR = os.getcwd() + "/target_data"
 ORIGIN_DIR = os.getcwd() + "/origin_data"
 
-if MODE == "PTB_TRAIN":        # PTB训练数据
-    RAW_DATA = ORIGIN_DIR + "/ptb.train.txt"  # 训练集数据文件
-    VOCAB = TARGET_DIR + "/ptb.vocab"                                 # 词汇表文件
-    OUTPUT_DATA = TARGET_DIR + "/ptb.train"                           # 将单词替换为单词编号后的输出文件
-elif MODE == "PTB_VALID":      # PTB验证数据
+if MODE == "PTB_TRAIN":        
+    RAW_DATA = ORIGIN_DIR + "/ptb.train.txt"  
+    VOCAB = TARGET_DIR + "/ptb.vocab"                                 
+    OUTPUT_DATA = TARGET_DIR + "/ptb.train"                           
+elif MODE == "PTB_VALID":      
     RAW_DATA = ORIGIN_DIR + "/ptb.valid.txt"
     VOCAB = TARGET_DIR + "/ptb.vocab"
     OUTPUT_DATA = TARGET_DIR + "/ptb.valid"
-elif MODE == "PTB_TEST":       # PTB测试数据
+elif MODE == "PTB_TEST":      
     RAW_DATA = ORIGIN_DIR + "/ptb.test.txt"
     VOCAB = TARGET_DIR + "/ptb.vocab"
     OUTPUT_DATA = TARGET_DIR + "/ptb.test"
-elif MODE == "TRANSLATE_ZH":   # 中文翻译数据
+elif MODE == "TRANSLATE_ZH":   
     RAW_DATA = ORIGIN_DIR + "/train.txt.zh.txt"
     VOCAB = TARGET_DIR + "/zh.vocab"
     OUTPUT_DATA = TARGET_DIR + "/train.zh"
-elif MODE == "TRANSLATE_EN":   # 英文翻译数据
+elif MODE == "TRANSLATE_EN":   
     RAW_DATA = ORIGIN_DIR + "/train.txt.en.txt"
     VOCAB = TARGET_DIR + "/en.vocab"
     OUTPUT_DATA = TARGET_DIR + "/train.en"
 
-# 读取词汇表，并建立词汇到单词编号的映射。
+
 with codecs.open(VOCAB, "r", "utf-8") as f_vocab:
     vocab = [w.strip() for w in f_vocab.readlines()]
 
 word_to_id = {k: v for (k, v) in zip(vocab, range(len(vocab)))}
-# 如果出现了不在词汇表内的低频词，则替换为"unk"。
 def get_id(word):
     return word_to_id[word] if word in word_to_id else word_to_id["<unk>"]
 
 fin = codecs.open(RAW_DATA, "r", "utf-8")
 fout = codecs.open(OUTPUT_DATA, 'w', 'utf-8')
 for line in fin:
-    words = line.strip().split() + ["<eos>"]  # 读取单词并添加<eos>结束符
-    # 将每个单词替换为词汇表中的编号
+    words = line.strip().split() + ["<eos>"]
     out_line = ' '.join([str(get_id(w)) for w in words]) + '\n'
     fout.write(out_line)
 fin.close()
--- create_word.py
+++ create_word.py
@@ -7,14 +7,14 @@ ORIGIN_DIR = os.getcwd() + "/origin_data"
 TARGET_DIR = os.getcwd() + "/target_data"
 MODE = "TRANSLATE_ZH"    # 将MODE设置为"PTB", "TRANSLATE_EN", "TRANSLATE_ZH"之一。
 
-if MODE == "PTB":             # PTB数据处理
-    RAW_DATA = ORIGIN_DIR+"/ptb.train.txt"  # 训练集数据文件
-    VOCAB_OUTPUT = TARGET_DIR + "/ptb.vocab"                         # 输出的词汇表文件
-elif MODE == "TRANSLATE_ZH":  # 翻译语料的中文部分
+if MODE == "PTB":            
+    RAW_DATA = ORIGIN_DIR+"/ptb.train.txt"  
+    VOCAB_OUTPUT = TARGET_DIR + "/ptb.vocab"                         
+elif MODE == "TRANSLATE_ZH":  
     RAW_DATA = ORIGIN_DIR + "/train.txt.zh.txt"
     VOCAB_OUTPUT = TARGET_DIR + "/zh.vocab"
     VOCAB_SIZE = 4000
-elif MODE == "TRANSLATE_EN":  # 翻译语料的英文部分
+elif MODE == "TRANSLATE_EN": 
     RAW_DATA = ORIGIN_DIR + "/train.txt.en.txt"
     VOCAB_OUTPUT = TARGET_DIR + "/en.vocab"
     VOCAB_SIZE = 10000
@@ -25,17 +25,13 @@ with codecs.open(RAW_DATA, "r", "utf-8") as f:
         for word in line.strip().split():
             counter[word] += 1
 
-# 按词频顺序对单词进行排序。
 sorted_word_to_cnt = sorted(
     counter.items(), key=itemgetter(1), reverse=True)
 sorted_words = [x[0] for x in sorted_word_to_cnt]
 
 if MODE == "PTB":
-    # 稍后我们需要在文本换行处加入句子结束符"<eos>"，这里预先将其加入词汇表。
     sorted_words = ["<eos>"] + sorted_words
 elif MODE in ["TRANSLATE_EN", "TRANSLATE_ZH"]:
-    # 在9.3.2小节处理机器翻译数据时，除了"<eos>"以外，还需要将"<unk>"和句子起始符
-    # "<sos>"加入词汇表，并从词汇表中删除低频词汇。
     sorted_words = ["<unk>", "<sos>", "<eos>"] + sorted_words
     if len(sorted_words) > VOCAB_SIZE:
         sorted_words = sorted_words[:VOCAB_SIZE]
